9eeb48856ef8c187b999c7072cf75d06f779d0de
Joaquin <joaquin@pop-os.localdomain>
2022-01-06 12:12:57 -0300

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
+const orderRoute = require('./routes/order');
+const cartRoute = require('./routes/cart');
 
 dotenv.config();
 
@@ -14,6 +17,9 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json())
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/products', productRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/carts', cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
